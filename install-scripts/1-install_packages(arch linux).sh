737856e23a753c0cd09003ef7f1d422974e20e97
helixoid <ayushrajmth91@gmail.com>
2024-03-01 12:30:36 +0530

    Install Script Improvements

--- install-scripts/1-install_packages(arch linux).sh
+++ install-scripts/1-install_packages(arch linux).sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Installing Necessary Packages
-paru -S --needed --noconfirm hyprland ironbar-git rofi-lbonn-wayland-git xdg-desktop-portal-hyprland linux-headers
+paru -S --needed --noconfirm hyprland waybar rofi-lbonn-wayland-git xdg-desktop-portal-hyprland linux-headers
 
 paru -S --needed --noconfirm nwg-look mpv yt-dlp mpd ncmpcpp cava
 
@@ -25,19 +25,19 @@ paru -S --needed --noconfirm slurp satty-bin newsboat bat lsd speech-dispatcher
 
 paru -S --needed --noconfirm papirus-icon-theme ttf-nerd-fonts-symbols
 
-#paru -S --needed --noconfirm thunar tumbler thunar-volman thunar-archive-plugin thunar-media-tags-plugin file-roller
+paru -S --needed --noconfirm thunar tumbler thunar-volman thunar-archive-plugin thunar-media-tags-plugin file-roller
 
 paru -S --needed --noconfirm gvfs gvfs-afc gvfs-mtp xdg-user-dirs ffmpegthumbnailer
 
 #paru -S --needed --noconfirm gst-plugins-good gst-plugins-bad gst-plugins-ugly
 
-#paru -S --needed --noconfirm flatpak xdg-desktop-portal-gtk avizo-git onefetch
+paru -S --needed --noconfirm flatpak xdg-desktop-portal-gtk
 
 paru -S --needed --noconfirm zathura zathura-pdf-mupdf zathura-cb yazi fd
 
-paru -S --needed --npconfirm fzf poppler zoxide ripgrep
+paru -S --needed --npconfirm fzf poppler zoxide ripgrep waycheck
 
-#paru -S --needed --noconfirm kvantum qt5ct qt6ct
+#paru -S --needed --noconfirm kvantum qt5ct qt6ct avizo-git onefetch
 
 #paru -S --needed --noconfirm sddm plymouth sddm-theme-sugar-candy-git
 
@@ -53,3 +53,11 @@ paru -Rns kitty
 paru -Rns xdg-desktop-portal-gnome
 paru -Rns xdg-desktop-portal-kde
 paru -c
+
+# Flatpak Apps Install
+flatpak install flathub com.github.tchx84.Flatseal -y
+flatpak install flathub io.missioncenter.MissionCenter -y
+flatpak install flathub dev.edfloreshz.Done -y
+flatpak install flathub info.febvre.Komikku -y
+flatpak install flathub org.telegram.desktop -y
+#flatpak install flathub org.kde.KStyle.Adwaita
