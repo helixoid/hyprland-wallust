cc4a406ca533dfff269631fc16ac1b7537acf385
helixoid <ayushrajmth91@gmail.com>
2024-03-04 00:37:05 +0530

    Added Intel One Mono (otf) Font

---
+++ .config/fish/config.fish
@@ -0,0 +1,13 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+macchina
+starship init fish | source
+alias v="nvim"
+alias hx="helix"
+alias prop="hyprctl clients | grep -i 'class\|title\|xwayland'"
+alias ls='lsd'
+alias l='ls -l'
+alias la='ls -a'
+alias lla='ls -la'
+alias lt='ls --tree'
+end
