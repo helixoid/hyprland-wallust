0b703251c9224bf4e358439fc9b8f2edc5cd134e
helixoid <ayushrajmth91@gmail.com>
2024-03-04 13:44:40 +0530

    Cava Now Follows the Colorscheme

--- install-scripts/2-install_dotfiles.sh
+++ install-scripts/2-install_dotfiles.sh
@@ -18,4 +18,7 @@ cp -R .config ~/
 cp -R Wallpapers ~/
 cd
 
+wallust run ~/Wallpaper/Akiakane-2.jpg
+
+ln -s ~/.cache/wal/cava ~/.config/cava/config
 #wget -qO- https://git.io/papirus-icon-theme-install | DESTDIR="$HOME/.local/share/icons" sh
