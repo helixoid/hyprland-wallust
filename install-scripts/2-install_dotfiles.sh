0ebd0cc879901c6374bd93765f7338a357a98e8a
helixoid <ayushrajmth91@gmail.com>
2024-03-06 20:45:34 +0530

    Updated everything!

--- install-scripts/2-install_dotfiles.sh
+++ install-scripts/2-install_dotfiles.sh
@@ -17,8 +17,3 @@ cd $HOME/hyprland-wallust
 cp -R .config ~/
 cp -R Wallpapers ~/
 cd
-
-wallust run ~/Wallpaper/Akiakane-2.jpg
-
-ln -s ~/.cache/wal/cava ~/.config/cava/config
-#wget -qO- https://git.io/papirus-icon-theme-install | DESTDIR="$HOME/.local/share/icons" sh
