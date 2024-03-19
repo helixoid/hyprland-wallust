9e6a825d35cb868d13c18aa618bec5ad51a34479
helixoid <ayushrajmth91@gmail.com>
2024-03-19 16:43:51 +0530

    Wezterm Now Follows the Color Scheme!

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,56 +1,20 @@
-local wezterm = require("wezterm")
-local act = wezterm.action
+local config = {}
 
-return {
-  adjust_window_size_when_changing_font_size = false,
-  check_for_updates = false,
-  window_background_opacity = 0.5,
-  command_palette_bg_color = "#44475a",
-  command_palette_fg_color = "#bd93f9",
-  color_scheme = "Catppuccin Mocha",
-  disable_default_key_bindings = true,
-  enable_tab_bar = false,
-  font = wezterm.font_with_fallback({
-    "JetBrains Mono",
-    "Symbols Nerd Font",
-    "Fira Code",
-    "Noto Sans Math",
-    "Noto Sans Symbols 2",
-    "Noto Sans Mono CJK JP",
-  }),
-  font_size = 13,
-  
-  -- List all keys with `wezterm show-keys --lua`.
-  keys = {
-    -- Copy/Paste
-    { key = "c", mods = "SHIFT|CTRL", action = act.CopyTo("Clipboard") },
-    { key = "v", mods = "SHIFT|CTRL", action = act.PasteFrom("Clipboard") },
-    { key = "Insert", mods = "SHIFT", action = act.PasteFrom("PrimarySelection") },
-    -- Copymode
-    { key = "x", mods = "SHIFT|CTRL", action = act.ActivateCopyMode },
-    -- Debug
-    { key = "d", mods = "SHIFT|CTRL", action = act.ShowDebugOverlay },
-    -- Font size
-    { key = "-", mods = "CTRL", action = act.DecreaseFontSize },
-    { key = "+", mods = "SHIFT|CTRL", action = act.IncreaseFontSize },
-    { key = "=", mods = "CTRL", action = act.ResetFontSize },
-    -- Help
-    { key = "h", mods = "SHIFT|CTRL", action = act.ActivateCommandPalette },
-    -- Quickselect
-    { key = "Space", mods = "SHIFT|CTRL", action = act.QuickSelect },
-    -- Scrollback
-    { key = "PageUp", action = act.ScrollByPage(-0.5) },
-    { key = "PageDown", action = act.ScrollByPage(0.5) },
-    {
-      key = "k",
-      mods = "SHIFT|CTRL",
-      action = act.Multiple({
-        act.ClearScrollback("ScrollbackAndViewport"),
-        act.SendKey({ key = "l", mods = "CTRL" }),
-      }),
-    },
-    -- Search
-    { key = "Slash", mods = "SHIFT|CTRL", action = act.Search("CurrentSelectionOrEmptyString") },
-  },
-  window_close_confirmation = "NeverPrompt",
-}
+config.enable_wayland = true
+config.front_end = "WebGpu"
+config.font = require("wezterm").font_with_fallback({
+  "Intel One Mono",
+  "Symbols Nerd Font",
+  "Noto Color Emoji"
+})
+config.font_size = 12
+
+config.color_scheme_dirs = { "~/.config/wezterm/colors" }
+config.color_scheme = "wallust"
+config.enable_tab_bar = false
+config.window_background_opacity = 0.25
+
+config.use_ime = true
+config.use_dead_keys = false
+
+return config
